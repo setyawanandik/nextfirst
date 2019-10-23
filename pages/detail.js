7b585e7b4bcd54d6209def0e050f7db8c3def79e
Andik Setyawan <andik.setyawan19@gmail.com>
2019-10-23 18:25:32 +0700

    fhjk

--- pages/detail.js
+++ pages/detail.js
@@ -1,16 +1,40 @@
 import React from 'react'
-//import Head from 'next/head'
+import Head from 'next/head'
+import Appbar from '../components/appbar'
+
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Botbar from '../components/botbar';
+import RecipeReviewCard from '../components/card';
+
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
-const Detail = () => {
-  const router = useRouter()
-  const { slug } = router.query
 
-  return <p>My Blog Post: {slug}</p>
-}
+const Detail = () => (
+    <div>
+        <Head>
+            <title>Detail</title>
+            <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Appbar />
+        <Container maxWidth="sm" style={{ marginTop: 160 }}>
+            <h1>Detail page</h1>
+            <Link href="/">
+                <a>Kembali Home</a>
+            </Link>
+        </Container>
+
+        <Container maxWidth="sm" style={{ marginTop: 60 }}>
+            <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        </Container>
+
+        <Botbar />
+        <style global jsx>{`
+        body {
+          background: #f7f7f7;
+        }
+      `}</style>
+    </div>
+)
 
 export default Detail
-
-
-
